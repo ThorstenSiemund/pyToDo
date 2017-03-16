c2a95a7cf5b24f0b7815a43f06726f9496594123
SiemThor <thorsten.siemund@siemens-ppal.com>
2017-03-16 14:02:59 +0900

    rework regex added logging added dummy function print_todos(todos(), add_todo(), delete_todo() added list functionality for {dd.m.yyyy} and {dd.m.yyyy}-{dd.m.yyyy} small clean ups

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import textwrap
 import datetime
 import re
+import sys
+import logging
 from csv import DictReader
 from distutils.util import strtobool
 from time import time
@@ -10,16 +12,25 @@ from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker
 
 
-REGEX_LIST_OPTION = r'\b(\d{1,2})([dw])\b'  # one or two digit followed by a char ([d,w])
-print(REGEX_LIST_OPTION)
-
-# REGEX_DATE = r'\d{2}\.\d{2}\.\d{4}'         # date: dd.mm.yyyyy
+logging.basicConfig(format='%(asctime)s %(levelname)s %(funcName)s[%(lineno)d] - %(message)s', level=logging.DEBUG)  # nopep8
 
 
-# REGEX_DATE_RANGE = REGEX_DATE + '\-' + REGEX_DATE
+# date: dd.mm.yyyyy
+REGEX_DATE = r'(\d{2}\.\d{2}\.\d{4})'
+# single date
+REGEX_DATE_SINGLE = '^' + REGEX_DATE + '$'
+# date range: dd.mm.yyyy-dd.mm.yyyy
+REGEX_DATE_RANGE = '^' + REGEX_DATE + '\-' + REGEX_DATE + '$'
 
-# rslt = re.match(REGEX_DATE_RANGE, '12.23.2345-12.23.4683')
-# print(rslt)
+
+# one or two digit followed by a char ([d,w])
+REGEX_LIST_OPTION = r'\b(\d{1,2})([dw])\b'
+# validating all list options
+REGEX_VALIDATE_LIST_OPTION = r'\ball\b|\bopen\b|\bdone\b' +\
+                             '|' + REGEX_LIST_OPTION + \
+                             '|' + REGEX_DATE_SINGLE +\
+                             '|' + REGEX_DATE_RANGE
+# print(REGEX_VALIDATE_LIST_OPTION)
 
 
 def fill_with_test_data(session):
@@ -42,11 +53,26 @@ def fill_with_test_data(session):
                         due_date=datetime.datetime.strptime(row['DueDate'], '%d.%m.%Y'),
                         description=row['Description'],)
             if session.query(ToDo).filter(ToDo.topic == todo.topic).count() > 0:
-                print('Topic alreadyexists: ' + todo.topic + ' ' + todo.due_date.strftime('%d.%m.%Y'))
+                print('Topic already exists: ' + todo.topic + ' ' + todo.due_date.strftime('%d.%m.%Y'))
             session.add(todo)
     print('Runtime: ', time() - start)
     session.commit()
 
+
+def print_todos(todos):
+    '''
+    print todos
+    '''
+    for todo in todos:
+        print(todo)
+
+
+def add_todo(session):
+    logging.debug('add todo')
+
+
+def delete_todo(session):
+    logging.debug('delete todo')
 
 def list_todos(session, list_option='all'):
     ''' This function list todos depending on <list_option>
@@ -61,10 +87,9 @@ def list_todos(session, list_option='all'):
     exceptions:
         RuntimeError: thrown if list_option is unknown
     '''
-    # TODO: list all todos for a date e.g. 17.03.2017
-    # TODO: list all todos for a special date range: 17.03.2017-23.03.2017
-    # list all todos - incl. thse are done
+    logging.debug('list todos')
     if list_option == 'all':
+        logging.debug('list all todos')
         for todo in session.query(ToDo):
             print(todo)
 
@@ -78,15 +103,36 @@ def list_todos(session, list_option='all'):
         for todo in session.query(ToDo).filter(ToDo.done == False):     # nopep8 E712
             print(todo)
 
+    # list all todos with a specific due date
+    elif re.search(REGEX_DATE_SINGLE, list_option):
+        print('\n', '-' * 10, ' ToDos specific due date ', '-' * 10)
+        print(list_option)
+        date1 = re.search(REGEX_DATE_SINGLE, list_option)  # get date
+        dt1 = datetime.datetime.strptime(date1.group(0), '%d.%m.%Y')
+        for todo in session.query(ToDo).filter(ToDo.due_date == dt1):
+            print(todo)
+
+    # list all todos within a specific date range
+    elif re.search(REGEX_DATE_RANGE, list_option):
+        print('\n', '-' * 10, ' ToDos within a date range ', '-' * 10)
+        date1 = re.search(REGEX_DATE_RANGE, list_option).group(1)    # get first date
+        date2 = re.search(REGEX_DATE_RANGE, list_option).group(2)    # get second date
+        dt1 = datetime.datetime.strptime(date1, '%d.%m.%Y')
+        dt2 = datetime.datetime.strptime(date2, '%d.%m.%Y')
+        for todo in session.query(ToDo).filter(and_(ToDo.due_date >= dt1, ToDo.due_date <= dt2)):
+            print(todo)
+
     # list all todos during the interval {x} days or {x}weeks
     elif re.search(REGEX_LIST_OPTION, list_option):
         regex = re.search(REGEX_LIST_OPTION, list_option)
         # day interval?
         if regex.group(2) == 'd':
+            # TODO: now should be 00:00
             td = (datetime.datetime.now() +
                   datetime.timedelta(days=int(regex.group(1))))
         # week interval?
         elif regex.group(2) == 'w':
+            # TODO: now should be 00:00
             td = (datetime.datetime.now() +
                   datetime.timedelta(week=int(regex.group(1))))
         # unknown list option
@@ -128,16 +174,17 @@ def create_session():
 
 def ListOptionString(v):
     try:
-        return re.match(r'\ball\b|\bopen\b|\bdone\b|(\d{1,2})([dw])', v).group(0)
+        # return re.match(r'\ball\b|\bopen\b|\bdone\b|(\d{1,2})([dw])', v).group(0)
+        return re.match(REGEX_VALIDATE_LIST_OPTION, v).group(0)
     except:
         raise argparse.ArgumentTypeError("String '%s' does not match required format" % (v,))
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Program for administrate a list fo ToDo.',
+    parser = argparse.ArgumentParser(description='Program for administrating a ToDo list.',
                                      usage='use "python %(prog)s --help" for more information',
                                      formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-a', '-add',
+    parser.add_argument('-a', '--add',
                         action="store_true",
                         dest='add',
                         default=False,
@@ -146,15 +193,15 @@ def main():
 
     parser.add_argument('-d', '--delete',
                         action='store_true',
-                        dest='delete_option',
+                        default=False,
                         required=False,
                         help='list ToDo topics')
 
     parser.add_argument('-l', '--list',
                         action='store',
                         type=ListOptionString,
-                        # choices=['all', 'open', 'done', '{x}d', '{x}w'],
                         dest='list_option',
+                        nargs='?',
                         default=None,
                         required=False,
                         help=textwrap.dedent('''\
@@ -162,27 +209,23 @@ def main():
                             all   list all ToDos including those which are done
                             open  list all open ToDos
                             done  list all done ToDos
-                            {x}d  list all open ToDos for he next {x} days (where {x} is 0..99)
-                            {x}w  list all open ToDos for he next {x} weeks (where {x} is 0..99)
+                            {x}d  list all open ToDos for he next {x} days (where {x} is 0..99 e.g. "2d", "15d", ...)
+                            {x}w  list all open ToDos for he next {x} weeks (where {x} is 0..99 e.g. "2w", "15w", ...)
                         ''')
                         )
-    # TODO: 'main.py -add' is throwing an exception
-    # parser.add_argument('-ids', nargs='*', dest='ids', help='some ids')
-    # print(parser.parse_args(['-h']))
     results = parser.parse_args()
-
-    # print('-a: ', results.add)
-    # print('-l: ', results.list_option)
-    # print('-ids', results.ids)
 
     session = create_session()
 
     fill_with_test_data(session)
 
     if results.list_option:
+        print(results.list_option)
         list_todos(session, results.list_option)
-
-    pass
+    elif results.add:
+        add_todo(session)
+    elif results.delete:
+        delete_todo(session)
 
 
 if __name__ == "__main__":
